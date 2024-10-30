3accfde6b39113febadb5a35683c106999e08bcb
Arman-Projects <newarmvardanyan221@gmail.com>
2024-10-30 20:58:34 +0400

    Update interproccesComunnication(Encode_decode).c

--- lecture6/interproccesComunnication(Encode_decode).c
+++ lecture6/interproccesComunnication(Encode_decode).c
@@ -6,10 +6,10 @@
 int res = 0;
 int ind = 0;
 
-void  alarm_handler (int )
-{
+void  alarm_handler (int ) {
     printf ("CHLP \n");
 }
+
 void sign(int sig) {
     if(sig == SIGUSR1) {
         printf("SIGNAL: %d SIGUSR1\n", sig);
@@ -27,10 +27,6 @@ int main () {
     if (pid < 0) {
         exit(1);
     } else if (pid == 0) {
-        struct sigaction s1;
-        struct sigaction s2;
-        sigfillset(&s1.sa_mask);
-        sigfillset(&s2.sa_mask);
         signal(SIGUSR1,sign);
         signal(SIGUSR2,sign);
         for (int i = 0; i < 32;  i++) {
@@ -41,30 +37,24 @@ int main () {
         
     } else {
         int num;
-        printf("print num: ");
+        printf("print num: \n");
         signal (SIGALRM, alarm_handler);
-        //scanf("%d", &num);
-        num = (((1 << 10) + 1) << 10) + 1;
-        sleep(2);
+        scanf("%d", &num);
         printf ("num=%d \n", num);
-        int i = 32;
-        while(i) {
+        int i = 31;
+        while(i != -1) {
             int tmp = num % 2;
             if(tmp) {
                 kill(pid,SIGUSR2);
-                //printf("%d",tmp);
             } else {
                 kill(pid, SIGUSR1);
-                //printf("%d",tmp);
             }
             pause ();
             num /= 2;
             --i;
         }
-                printf("\n");
     wait(NULL);
     }
 
     return 0;
 }
-
